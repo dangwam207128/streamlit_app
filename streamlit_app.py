c6af203cb4285099b5fbe351438cf41ff879e90d
Mayank Dangwal <111003998+dangwam207128@users.noreply.github.com>
2022-08-10 16:12:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-streamlit.title( 🍞 "All about Healthy Food")
+streamlit.title(' 🍞 All about Healthy Food')
 
-streamlit.header(🥣 'Breakfast Menu')
-streamlit.text(🥗 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥑 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
   
