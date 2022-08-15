411e52bd6451972ab56b3bc75d0e6ce84b070a23
Mayank Dangwal <111003998+dangwam207128@users.noreply.github.com>
2022-08-15 10:53:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,5 @@ my_cur.execute("select current_user(),current_account(),current_region()")
 my_data_row=my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
-streamlit.text(my_cur)
+streamlit.text(**my_cur)
 
